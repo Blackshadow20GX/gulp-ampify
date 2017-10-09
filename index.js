41c4815ecf0cebcd29163aa4733c4dec98233de9
Marc Hanley <marcjameshanley@gmail.com>
2017-10-09 15:19:43 +1030

    Update package name

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var ampify = require('ampify');
 
-//Gulp wrapper plugin for html-to-amp
+//Gulp wrapper plugin for Amplify package
 module.exports = function(directory){
   return through.obj(function(file, enc, cb) {
     if(file.isNull()) return cb(null, file);
     if(file.isStream()) return cb(new Error('gulp-html-to-amp: Streams not supported.'));
 
     const html = file.contents.toString();
-    gutil.log('HTML: ' + html);
-    var amp = ampify(html, {cwd: directory});
-    amp = amp.toString();
-    gutil.log('AMP:' + amp);
+    const amp = ampify(html, {cwd: directory});
     file.contents = new Buffer(amp);
     cb(null, file);
   });
